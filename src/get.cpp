e9ddd43ca50feaf0d9b8d88159995bba15c2ca8d
derikon <derikon@yandex-team.ru>
2014-03-31 22:00:24 +0400

    Removed embeds

--- src/get.cpp
+++ src/get.cpp
@@ -32,7 +32,6 @@ void proxy::req_get::on_request(const ioremap::swarm::http_request &req, const b
 	auto query_list = req.url().query();
 	m_offset = get_arg<uint64_t>(query_list, "offset", 0);
 	m_size = get_arg<uint64_t>(query_list, "size", 0);
-	m_embed = query_list.has_item("embed") || query_list.has_item("embed_timestamp");
 	m_if_modified_since = req.headers().get("If-Modified-Since");
 	m_first_chunk = true;
 	m_chunk_size = server()->m_read_chunk_size;
@@ -101,24 +100,14 @@ void proxy::req_get::on_read_chunk(const ioremap::elliptics::sync_read_result &s
 	const auto &rr = srr.front();
 	auto file = rr.file();
 
-	m_embed = false;
-	if (rr.io_attribute()->user_flags & UF_EMBEDS) {
-		m_embed = true;
-	}
-
-	std::string data;
+	std::string data = file.to_string();
 
 	if (m_first_chunk) {
 		m_first_chunk = false;
 
-		auto dc = elliptics::data_container_t::unpack(file, m_embed);
-		auto ts = dc.get<elliptics::DNET_FCGI_EMBED_TIMESTAMP>();
-
-		dc.data.to_string().swap(data);
-
 		ioremap::swarm::http_response reply;
 		reply.set_code(200);
-		reply.headers().set_content_length(m_size - file.size() + data.size());
+		reply.headers().set_content_length(m_size);
 
 		if (NULL == server()->m_magic.get()) {
 			server()->m_magic.reset(new magic_provider());
@@ -127,12 +116,7 @@ void proxy::req_get::on_read_chunk(const ioremap::elliptics::sync_read_result &s
 		reply.headers().set_content_type(server()->m_magic->type(data));
 
 		{
-			time_t timestamp = 0;
-			if (ts) {
-				timestamp = (time_t)(ts->tv_sec);
-			} else {
-				timestamp = (time_t)(rr.io_attribute()->timestamp.tsec);
-			}
+			time_t timestamp = (time_t)(rr.io_attribute()->timestamp.tsec);
 
 			char ts_str[128] = {0};
 			struct tm tmp;
@@ -149,8 +133,6 @@ void proxy::req_get::on_read_chunk(const ioremap::elliptics::sync_read_result &s
 		}
 
 		send_headers(std::move(reply), std::function<void (const boost::system::error_code &)>());
-	} else {
-		file.to_string().swap(data);
 	}
 
 	send_data(std::move(data), std::bind(&proxy::req_get::on_sent_chunk, shared_from_this(), std::placeholders::_1));
