309c9790ac942290e1d553899c504aa37a4ccb53
derikon <derikon@yandex-team.ru>
2014-12-16 21:02:16 +0400

    Writer class is added

---
+++ src/writer.cpp
@@ -0,0 +1,415 @@
+/*
+	Mediastorage-proxy is a HTTP proxy for mediastorage based on elliptics
+	Copyright (C) 2013-2014 Yandex
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#include "writer.hpp"
+
+#include "loggers.hpp"
+#include "utils.hpp"
+#include "lookup_result.hpp"
+
+const char *
+elliptics::writer::error_category_t::name() const {
+	return "writer error category";
+}
+std::string
+elliptics::writer::error_category_t::message(int ev) const {
+	switch (ev) {
+		// TODO: add descriptions for each error code
+	default:
+		return "unknown error";
+	}
+}
+
+const std::error_category &
+elliptics::writer::error_category() {
+	const static error_category_t instance;
+	return instance;
+}
+
+std::error_code
+elliptics::writer::make_error_code(error_tag e) {
+	return std::error_code(static_cast<int>(e), error_category());
+}
+
+std::error_condition
+elliptics::writer::make_error_condition(error_tag e) {
+	return std::error_condition(static_cast<int>(e), error_category());
+}
+
+elliptics::writer::unexpected_event_error::unexpected_event_error(const std::string &message)
+	: std::system_error(make_error_code(error_tag::unexpected_event), message)
+{
+}
+
+elliptics::writer::incorrect_size_error::incorrect_size_error(const std::string &message)
+	: std::system_error(make_error_code(error_tag::incorrect_size), message)
+{
+}
+
+elliptics::writer::internal_error::internal_error(const std::string &message)
+	: std::system_error(make_error_code(error_tag::internal), message)
+{
+}
+
+elliptics::writer::writer_t::writer_t(ioremap::swarm::logger bh_logger_
+		, const ioremap::elliptics::session &session_, std::string key_
+		, size_t total_size_, size_t offset_, size_t commit_coef_, size_t success_copies_num_
+		, callback_t on_complete_
+		)
+	: state(state_tag::waiting)
+	, bh_logger(std::move(bh_logger_))
+	, session(session_.clone())
+	, key(std::move(key_))
+	, total_size(total_size_)
+	, offset(offset_)
+	, commit_coef(commit_coef_)
+	, success_copies_num(success_copies_num_)
+	, on_complete(std::move(on_complete_))
+	, written_size(0)
+	, start_time(std::chrono::system_clock::now())
+{
+	session.set_filter(ioremap::elliptics::filters::all_with_ack);
+	session.set_checker(ioremap::elliptics::checkers::at_least_one);
+
+	key.transform(session);
+	key.set_id(key.id());
+
+	{
+		std::ostringstream oss;
+		oss
+			<< "writing starts:"
+			<< " key=" << key.remote()
+			// The only reason to print elliptics key is ioremap::elliptics::key::transform
+			// method doesn't log this information
+			<< " elliptics-key=" << key.to_string()
+			<< " offset=" << offset
+			<< " total-size=" << total_size
+			<< " groups=" << session.get_groups()
+			<< " success-copiens-num=" << success_copies_num;
+
+		auto msg = oss.str();
+
+		MDS_LOG_INFO("%s", msg.c_str());
+	}
+}
+
+void
+elliptics::writer::writer_t::write(const char *data, size_t size) {
+	write(ioremap::elliptics::data_pointer::from_raw(
+			reinterpret_cast<void *>(const_cast<char *>(data)), size));
+}
+
+void
+elliptics::writer::writer_t::write(const ioremap::elliptics::data_pointer &data_pointer) {
+	lock_guard_t lock_guard(state_mutex);
+	(void) lock_guard;
+
+	switch (state) {
+	case state_tag::waiting: {
+		size_t future_size = data_pointer.size() + written_size;
+
+		if (future_size > total_size) {
+			throw incorrect_size_error();
+		}
+
+		if (written_size == 0 && data_pointer.size() == total_size) {
+			log_chunk("simple", data_pointer.size());
+			auto async_result = session.write_data(key, data_pointer, offset);
+			written_size = data_pointer.size();
+
+			// Actually state should be changed immediately before return
+			// But connect can call callback synchronously
+			state = state_tag::committing;
+
+			async_result.connect(std::bind(&writer_t::on_data_wrote
+						, shared_from_this(), std::placeholders::_1, std::placeholders::_2));
+			return;
+		}
+
+		auto async_result = write_impl(data_pointer);
+		written_size += data_pointer.size();
+		offset += data_pointer.size();
+
+		async_result.connect(std::bind(&writer_t::on_data_wrote
+					, shared_from_this(), std::placeholders::_1, std::placeholders::_2));
+		break;
+	}
+	case state_tag::writing:
+	case state_tag::committing:
+	case state_tag::need_remove:
+	case state_tag::removing:
+	case state_tag::committed:
+	case state_tag::failed:
+		throw unexpected_event_error();
+	}
+}
+
+const elliptics::writer::writer_t::entries_info_t &
+elliptics::writer::writer_t::result() const {
+	lock_guard_t lock_guard(state_mutex);
+	(void) lock_guard;
+
+	switch (state) {
+	case state_tag::committed:
+		return entries_info;
+	case state_tag::waiting:
+	case state_tag::writing:
+	case state_tag::committing:
+	case state_tag::need_remove:
+	case state_tag::removing:
+	case state_tag::failed:
+	// Default is needed only for avoding compile warning:
+	// 'control reaches end of non-void function'
+	default:
+		throw unexpected_event_error();
+	}
+}
+
+bool
+elliptics::writer::writer_t::is_finished() const {
+	lock_guard_t lock_guard(state_mutex);
+	(void) lock_guard;
+
+	return state == state_tag::committed
+		|| state == state_tag::failed;
+}
+
+bool
+elliptics::writer::writer_t::is_committed() const {
+	lock_guard_t lock_guard(state_mutex);
+	(void) lock_guard;
+
+	return state == state_tag::committed;
+}
+
+bool
+elliptics::writer::writer_t::is_failed() const {
+	lock_guard_t lock_guard(state_mutex);
+	(void) lock_guard;
+
+	return state == state_tag::failed;
+}
+
+ioremap::swarm::logger &
+elliptics::writer::writer_t::logger() {
+	return bh_logger;
+}
+
+void
+elliptics::writer::writer_t::log_chunk(
+		const std::string &write_type, size_t chunk_size) {
+	std::ostringstream oss;
+	oss
+		<< "write chunk:"
+		<< " key=" << key.remote()
+		<< " total-size=" << total_size
+		<< " offset=" << offset
+		<< " chunk-size=" << chunk_size
+		<< " data-left=" << (total_size - offset)
+		<< " write-type=" << write_type;
+
+	auto msg = oss.str();
+
+	MDS_LOG_INFO("%s", msg.c_str());
+}
+
+void
+elliptics::writer::writer_t::update_groups(
+		const ioremap::elliptics::sync_write_result &entries) {
+	std::vector<int> good_groups;
+
+	for (auto it = entries.begin(), end = entries.end(); it != end; ++it) {
+		auto &entry = *it;
+
+		int group_id = entry.command()->id.group_id;
+
+		if (entry.status() == 0) {
+			good_groups.emplace_back(group_id);
+		} else {
+			bad_groups.emplace_back(group_id);
+		}
+	}
+
+	session.set_groups(good_groups);
+
+	{
+		std::ostringstream oss;
+		oss
+			<< "writing of chunk is finished:"
+			<< " key=" << key.remote()
+			<< " good-groups=" << good_groups
+			<< " bad-groups=" << bad_groups;
+
+		auto msg = oss.str();
+
+		MDS_LOG_INFO("%s", msg.c_str());
+	}
+}
+
+void
+elliptics::writer::writer_t::set_result(
+		const ioremap::elliptics::sync_write_result &entries) {
+	for (auto it = entries.begin(), end = entries.end(); it != end; ++it) {
+		lookup_result pl(*it, "");
+		if (pl.status() != 0) {
+			continue;
+		}
+
+		entry_info_t entry_info;
+
+		entry_info.address = pl.addr();
+		entry_info.path = pl.full_path();
+		entry_info.group = pl.group();
+
+		entries_info.emplace_back(std::move(entry_info));
+	}
+}
+
+bool
+elliptics::writer::writer_t::write_is_good(
+		const ioremap::elliptics::error_info &error_info) {
+	return !error_info && session.get_groups().size() >= success_copies_num;
+}
+
+ioremap::elliptics::async_write_result
+elliptics::writer::writer_t::write_impl(
+		const ioremap::elliptics::data_pointer &data_pointer) {
+	if (written_size == 0) {
+		log_chunk("prepare", data_pointer.size());
+		return session.write_prepare(key, data_pointer, offset, total_size);
+	} else {
+		size_t future_size = written_size + data_pointer.size();
+
+		if (future_size > total_size) {
+			throw incorrect_size_error();
+		}
+
+		if (future_size == total_size) {
+			if (commit_coef) {
+				session.set_timeout(session.get_timeout() + total_size / commit_coef);
+			}
+			log_chunk("commit", data_pointer.size());
+			return session.write_commit(key, data_pointer, offset, future_size);
+		} else {
+			log_chunk("plain", data_pointer.size());
+			return session.write_plain(key, data_pointer, offset);
+		}
+	}
+}
+
+void
+elliptics::writer::writer_t::on_data_wrote(
+		const ioremap::elliptics::sync_write_result &entries
+		, const ioremap::elliptics::error_info &error_info) {
+#define LOG_RESULT(VERBOSITY, STATUS) \
+	do { \
+		auto spent_time = std::chrono::duration_cast<std::chrono::milliseconds>( \
+				std::chrono::system_clock::now() - start_time).count(); \
+		 \
+		std::ostringstream oss; \
+		oss \
+			<< "writing is finished:" \
+			<< " key=" << key.remote() \
+			<< " spent-time=" << spent_time << "ms" \
+			<< " status=" << STATUS \
+			<< " wrote into groups " << session.get_groups() \
+			<< " failed to write into groups " << bad_groups; \
+		 \
+		auto msg = oss.str(); \
+		MDS_LOG_##VERBOSITY("%s", msg.c_str()); \
+	} while (0)
+
+	lock_guard_t lock_guard(state_mutex);
+	(void) lock_guard;
+
+	switch (state) {
+	case state_tag::writing:
+	case state_tag::committing: {
+		update_groups(entries);
+
+		if (write_is_good(error_info)) {
+			if (state == state_tag::committing) {
+				LOG_RESULT(INFO, "ok");
+				set_result(entries);
+				state = state_tag::committed;
+			} else {
+				state = state_tag::waiting;
+			}
+
+			on_complete(make_error_code(error_tag::success));
+			return;
+		}
+
+		LOG_RESULT(ERROR, "bad");
+
+		if (state == state_tag::committing) {
+			state = state_tag::removing;
+			{
+				std::ostringstream oss;
+				oss
+					<< "remove start:"
+					<< " key=" << key.remote()
+					<< " groups=" << session.get_groups()
+					;
+				auto msg = oss.str();
+				MDS_LOG_INFO("%s", msg.c_str());
+			}
+
+			// TODO: need to set remove-timeout
+			auto async_result = session.remove(key);
+			async_result.connect(std::bind(&writer_t::on_data_removed
+						, shared_from_this(), std::placeholders::_1, std::placeholders::_2));
+		}
+		break;
+	}
+	case state_tag::waiting:
+	case state_tag::need_remove:
+	case state_tag::removing:
+	case state_tag::committed:
+	case state_tag::failed:
+		throw unexpected_event_error();
+	}
+
+#undef LOG_RESULT
+}
+
+void
+elliptics::writer::writer_t::on_data_removed(
+		const ioremap::elliptics::sync_remove_result &entries
+		, const ioremap::elliptics::error_info &error_info) {
+	lock_guard_t lock_guard(state_mutex);
+	(void) lock_guard;
+
+	switch (state) {
+	case state_tag::removing: {
+		MDS_LOG_INFO("remove is finished");
+		state = state_tag::failed;
+		on_complete(make_error_code(error_tag::internal));
+		break;
+	}
+	case state_tag::waiting:
+	case state_tag::writing:
+	case state_tag::committing:
+	case state_tag::need_remove:
+	case state_tag::committed:
+	case state_tag::failed:
+		throw unexpected_event_error();
+	}
+}
+
---
+++ src/writer.hpp
@@ -0,0 +1,192 @@
+/*
+	Mediastorage-proxy is a HTTP proxy for mediastorage based on elliptics
+	Copyright (C) 2013-2014 Yandex
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#ifndef MDS_PROXY__SRC__UPLOADER__HPP
+#define MDS_PROXY__SRC__UPLOADER__HPP
+
+#include <elliptics/session.hpp>
+
+#include <swarm/logger.hpp>
+
+#include <memory>
+#include <functional>
+#include <system_error>
+#include <mutex>
+
+namespace elliptics { namespace writer {
+
+enum class error_tag {
+	  success
+	, unexpected_event
+	, incorrect_size
+	, internal
+};
+
+class error_category_t
+	: public std::error_category
+{
+public:
+	const char *
+	name() const;
+
+	std::string
+	message(int ev) const;
+};
+
+const std::error_category &
+error_category();
+
+std::error_code
+make_error_code(error_tag e);
+
+std::error_condition
+make_error_condition(error_tag e);
+
+class unexpected_event_error : public std::system_error
+{
+public:
+	unexpected_event_error(const std::string &message = "");
+};
+
+class incorrect_size_error : public std::system_error
+{
+public:
+	incorrect_size_error(const std::string &message = "");
+};
+
+class internal_error : public std::system_error
+{
+public:
+	internal_error(const std::string &message = "");
+};
+
+class writer_t : public std::enable_shared_from_this<writer_t>
+{
+public:
+	typedef std::function<void (const std::error_code &)> callback_t;
+
+	struct entry_info_t {
+		std::string address;
+		std::string path;
+		int group;
+	};
+
+	typedef std::vector<entry_info_t> entries_info_t;
+
+	writer_t(ioremap::swarm::logger bh_logger_
+			, const ioremap::elliptics::session &session_, std::string key_
+			, size_t total_size_, size_t offset_, size_t commit_coef_, size_t success_copies_num_
+			, callback_t on_complete_
+			);
+
+	void
+	write(const char *data, size_t size);
+
+	void
+	write(const ioremap::elliptics::data_pointer &data_pointer);
+
+	const entries_info_t &
+	result() const;
+
+	bool
+	is_finished() const;
+
+	bool
+	is_committed() const;
+
+	bool
+	is_failed() const;
+
+private:
+	enum class state_tag {
+		  waiting
+		, writing
+		, committing
+		, need_remove
+		, removing
+		, committed
+		, failed
+	};
+
+	typedef std::recursive_mutex mutex_t;
+	typedef std::lock_guard<mutex_t> lock_guard_t;
+
+	ioremap::swarm::logger &
+	logger();
+
+	void
+	log_chunk(const std::string &write_type, size_t chunk_size);
+
+	void
+	update_groups(const ioremap::elliptics::sync_write_result &entries);
+
+	void
+	set_result(const ioremap::elliptics::sync_write_result &entries);
+
+	bool
+	write_is_good(const ioremap::elliptics::error_info &error_info);
+
+	ioremap::elliptics::async_write_result
+	write_impl(const ioremap::elliptics::data_pointer &data_pointer);
+
+	void
+	on_data_wrote(const ioremap::elliptics::sync_write_result &entries
+			, const ioremap::elliptics::error_info &error_info);
+
+	void
+	on_data_removed(const ioremap::elliptics::sync_remove_result &entries
+			, const ioremap::elliptics::error_info &error_info);
+
+	state_tag state;
+	mutable mutex_t state_mutex;
+
+	ioremap::swarm::logger bh_logger;
+
+	ioremap::elliptics::session session;
+	ioremap::elliptics::key key;
+
+	size_t total_size;
+	size_t offset;
+	size_t commit_coef;
+	size_t success_copies_num;
+
+	callback_t on_complete;
+
+	size_t written_size;
+	std::vector<int> bad_groups;
+
+	std::chrono::system_clock::time_point start_time;
+
+	entries_info_t entries_info;
+};
+
+
+}} // namespace elliptics::writer
+
+namespace std {
+
+template <>
+struct is_error_code_enum<elliptics::writer::error_tag>
+	: public true_type
+{};
+
+} // namespace std
+
+#endif /* MDS_PROXY__SRC__UPLOADER__HPP */
+
